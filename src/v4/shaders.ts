c62885f3f900f182f7a39d48caf31061562fdc8b
byronbuzz <115992627+byronbuzz@users.noreply.github.com>
2026-08-02 20:02:20 +1000

    Enable row-offset compute dispatches

--- src/v4/shaders.ts
+++ src/v4/shaders.ts
@@ -1,2 +1,8 @@
-export { computeShader, presentShader } from './shadersBase';
+import { computeShader as baseComputeShader } from './shadersBase';
+
+export const computeShader = baseComputeShader
+  .replace('  _pad0: u32,\n}', '  rowStart: u32,\n}')
+  .replace('  let id = gid.xy;\n', '  let id = vec2u(gid.x, gid.y + p.rowStart);\n');
+
+export { presentShader } from './shadersBase';
 export { composeShader } from './composeShader';
